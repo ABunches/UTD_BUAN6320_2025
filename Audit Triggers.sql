94c0f394be92385712c9e78b80056f6b4a51658e
ABunches <97646371+ABunches@users.noreply.github.com>
2025-04-29 01:40:44 -0500

    Update Audit Triggers.sql

--- Audit Triggers.sql
+++ Audit Triggers.sql
@@ -1,9 +1,8 @@
 USE Contoso;
-SELECT * FROM contoso.store;
 
-
-CREATE TRIGGER Orders_set_random_updated_by
-BEFORE UPDATE ON orders
+DELIMITER //
+CREATE TRIGGER currencyexchange_set_random_updated_by
+BEFORE UPDATE ON contoso.currencyexchange
 FOR EACH ROW
 BEGIN
   DECLARE random_username VARCHAR(100);
@@ -17,3 +16,156 @@ BEGIN
 
   SET NEW.UpdatedBy = random_username;
 END;
+// DELIMITER ;
+
+
+CREATE TRIGGER customer_set_random_updated_by
+BEFORE UPDATE ON contoso.customer
+FOR EACH ROW
+BEGIN
+  DECLARE random_username VARCHAR(100);
+
+  SELECT UserName
+  INTO random_username
+  FROM Users
+  WHERE UserKey <> 1 #Admin
+  ORDER BY RAND()
+  LIMIT 1;
+
+  SET NEW.UpdatedBy = random_username;
+END;
+
+
+CREATE TRIGGER geoarea_set_random_updated_by
+BEFORE UPDATE ON contoso.geoarea
+FOR EACH ROW
+BEGIN
+  DECLARE random_username VARCHAR(100);
+
+  SELECT UserName
+  INTO random_username
+  FROM Users
+  WHERE UserKey <> 1 #Admin
+  ORDER BY RAND()
+  LIMIT 1;
+
+  SET NEW.UpdatedBy = random_username;
+END;
+
+
+CREATE TRIGGER orderrows_set_random_updated_by
+BEFORE UPDATE ON contoso.orderrows
+FOR EACH ROW
+BEGIN
+  DECLARE random_username VARCHAR(100);
+
+  SELECT UserName
+  INTO random_username
+  FROM Users
+  WHERE UserKey <> 1 #Admin
+  ORDER BY RAND()
+  LIMIT 1;
+
+  SET NEW.UpdatedBy = random_username;
+END;
+
+
+CREATE TRIGGER orders_set_random_updated_by
+BEFORE UPDATE ON contoso.orders
+FOR EACH ROW
+BEGIN
+  DECLARE random_username VARCHAR(100);
+
+  SELECT UserName
+  INTO random_username
+  FROM Users
+  WHERE UserKey <> 1 #Admin
+  ORDER BY RAND()
+  LIMIT 1;
+
+  SET NEW.UpdatedBy = random_username;
+END;
+
+
+CREATE TRIGGER product_set_random_updated_by
+BEFORE UPDATE ON contoso.product
+FOR EACH ROW
+BEGIN
+  DECLARE random_username VARCHAR(100);
+
+  SELECT UserName
+  INTO random_username
+  FROM Users
+  WHERE UserKey <> 1 #Admin
+  ORDER BY RAND()
+  LIMIT 1;
+
+  SET NEW.UpdatedBy = random_username;
+END;
+
+
+CREATE TRIGGER productcategory_set_random_updated_by
+BEFORE UPDATE ON contoso.productcategory
+FOR EACH ROW
+BEGIN
+  DECLARE random_username VARCHAR(100);
+
+  SELECT UserName
+  INTO random_username
+  FROM Users
+  WHERE UserKey <> 1 #Admin
+  ORDER BY RAND()
+  LIMIT 1;
+
+  SET NEW.UpdatedBy = random_username;
+END;
+
+
+CREATE TRIGGER sales_set_random_updated_by
+BEFORE UPDATE ON contoso.sales
+FOR EACH ROW
+BEGIN
+  DECLARE random_username VARCHAR(100);
+
+  SELECT UserName
+  INTO random_username
+  FROM Users
+  WHERE UserKey <> 1 #Admin
+  ORDER BY RAND()
+  LIMIT 1;
+
+  SET NEW.UpdatedBy = random_username;
+END;
+
+CREATE TRIGGER store_set_random_updated_by
+BEFORE UPDATE ON contoso.store
+FOR EACH ROW
+BEGIN
+  DECLARE random_username VARCHAR(100);
+
+  SELECT UserName
+  INTO random_username
+  FROM Users
+  WHERE UserKey <> 1 #Admin
+  ORDER BY RAND()
+  LIMIT 1;
+
+  SET NEW.UpdatedBy = random_username;
+END;
+
+CREATE TRIGGER users_set_random_updated_by
+BEFORE UPDATE ON contoso.users
+FOR EACH ROW
+BEGIN
+  DECLARE random_username VARCHAR(100);
+
+  SELECT UserName
+  INTO random_username
+  FROM Users
+  WHERE UserKey <> 1 #Admin
+  ORDER BY RAND()
+  LIMIT 1;
+
+  SET NEW.UpdatedBy = random_username;
+END;
+
